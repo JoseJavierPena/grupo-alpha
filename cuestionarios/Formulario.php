58a76ce8adecdb5943edd9b45b7c107a8b79bac5
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2021-10-22 16:23:25 -0400

    php

--- cuestionarios/Formulario.php
+++ cuestionarios/Formulario.php
@@ -1,17 +1,29 @@
 <?
-$Fist_name=$_POST['Fist_name'];
-$Last_name=$_POST['Last_name'];
-$Id=$_POST['Id'];
-$College=$_POST['College'];
-$Cellphone=$_POST['Cellphone'];
-$Address=$_POST['Address'];
-$Location=$_POST['Location'];
-$Imail=$_POST['Imail'];
+include 'db.php';
 
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+$cedula=$_POST['cedula'];
+$universidad=$_POST['universidad'];
+$telefono=$_POST['telefono'];
+$direccion=$_POST['direccion'];
+$donde_vives=$_POST['donde_vives'];
+$correo=$_POST['correo'];
 
+$sql = "INSERT INTO  cliente values('$nombre','$apellido','$cedula','$universidad','$telefono',
+'$direccion','$donde_vives','$correo')";
+ $ejecutar = mysqli_query ($conexion,$sql) or die (mysqli_error($conexion));
+ 
 
-
-
+ if (!$ejecutar){
+     echo  "Hay un error";
+ }
+ else {
+     echo "Datos guardados";
+ }
+ $_SESSION['message'] = 'SR/SRA '.$nombre.' Usted ha sido Registrado/a';
+ $_SESSION['message_type'] = 'success';
+  
 
 
 ?>
