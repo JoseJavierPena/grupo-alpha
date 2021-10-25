67d2a6888c2a39101f20134334a850de7c2deb5c
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2021-10-25 11:16:11 -0400

    php

--- cuestionarios/Formulario.php
+++ cuestionarios/Formulario.php
@@ -1,5 +1,5 @@
-<?
-include 'db.php';
+<?php
+
 
 $nombre=$_POST['nombre'];
 $apellido=$_POST['apellido'];
@@ -9,21 +9,23 @@ $telefono=$_POST['telefono'];
 $direccion=$_POST['direccion'];
 $donde_vives=$_POST['donde_vives'];
 $correo=$_POST['correo'];
-
-$sql = "INSERT INTO  cliente values('$nombre','$apellido','$cedula','$universidad','$telefono',
+//base de datos
+$conexion=mysqli_connect("localhost:3310","root","","pasantes alpha db");
+$sql = "INSERT INTO  formulario values(id,'$nombre','$apellido','$cedula','$universidad','$telefono',
 '$direccion','$donde_vives','$correo')";
  $ejecutar = mysqli_query ($conexion,$sql) or die (mysqli_error($conexion));
- 
 
  if (!$ejecutar){
-     echo  "Hay un error";
- }
- else {
-     echo "Datos guardados";
- }
- $_SESSION['message'] = 'SR/SRA '.$nombre.' Usted ha sido Registrado/a';
- $_SESSION['message_type'] = 'success';
-  
+    echo  "Hay un error";
+}
+else {
+    echo "Datos guardados";
+}
 
+
+ 
+  
+  
+ 
 
 ?>
