ec1ed2903e9c0afe43a850e082f64fb2f189af0f
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2021-10-27 12:16:50 -0400

    php

--- cuestionarios/evaluaciones individuales.php
+++ cuestionarios/evaluaciones individuales.php
@@ -1,33 +1,46 @@
 <?php
-session_start();
-$conexion=mysqli_connect("localhost:8111","root","","pasantes alpha db");
+$conexion=mysqli_connect("localhost:3310","root","","pasantes alpha db");
+$question1="";
+$question2="";
+$question3="";
+$question4="";
+$question5="";
+$question6="";
+if(isset($_POST['question1'])){
+    $question1=$_POST['question1'];
+}elseif(isset($_POST['question2'])){
+    $question2=$_POST['question2'];
+}elseif(isset($_POST['question3'])){
+    $question3=$_POST['question3'];
+}elseif(isset($_POST['question4'])){
+    $question4=$_POST['question4'];
+}elseif(isset($_POST['question5'])){
+    $question5=$_POST['question5'];
+}elseif(isset($_POST['question6'])){
+    $question6=$_POST['question6'];
+}
 if(isset($_POST['enviar'])){
-$question1 = $_POST['question1'];
-$question2 = $_POST['question2'];
-$question3 = $_POST['question3'];
-$question4 = $_POST['question4'];
-$question5 = $_POST['question5'];
-$question6 = $_POST['question6'];
+$question1 = $_REQUEST['question1'];
+$question2 = $_REQUEST['question2'];
+$question3 = $_REQUEST['question3'];
+$question4 = $_REQUEST['question4'];
+$question5 = $_REQUEST['question5'];
+$question6 = $_REQUESTr['question6'];
 
 
-$query="INSERT INTO evaluacionesindividuales(Conocimientos_HTML,Conocimientos_CSS,Conocimientos_JavaScript,Conocimientos_PHP,ConocimientosSQLMySQL,Otroslenguajesprogramación) values('$question1','$question2','$question3','$question4','$question5','$question6')";
-$query_run= mysqli_query($conexion,$query);
-if($query_run){
-    $_SESSION['status'] = 'datos ingresados correctamente';
-    header("location : evaluaciones individuales.html");
-}
-else{
-    $_SESSION['status'] = 'datos ingresados correctamente';
-    header("location : evaluaciones individuales.html");
-}
-}
+$insertar="INSERT INTO evaluacionesIndividuales(Conocimientos_HTML,Conocimientos_CSS,Conocimientos_JavaScript,Conocimientos_PHP,ConocimientosSQLMySQL,Otroslenguajesprogramacion) values('$question1','$question2','$question3','$question4','$question5','$question6')";
+$resultado= mysqli_query($conexion,$insertar);
 
 
-
-
-
-
-
+if($resultado){
+    echo "<script> alert('Se enviaron los datos');</script>";
+    header("location:evaluaciones individuales.html");
+    }else{
+        print("Errormessage: %s\n". mysqli_error($conexion));
+    
+    }
+        }
+      
 
 
 
