b09ea1006966f6eaa1e3653a2f774c9f70eeb37f
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2021-11-02 16:32:14 -0400

    php

--- cuestionarios/envio.php
+++ cuestionarios/envio.php
@@ -1,7 +1,17 @@
 <?php
 if(isset($_POST['enviar'])){
-    if(!empty($_POST['nombre']) && !empty($_POST['asunto']) && !empty($_POST['mensaje']) ){
-        
+    if(!empty($_POST['nombre']) && !empty($_POST['asunto']) && !empty($_POST['mensaje']) && !empty($_POST['correo']) ){
+        $nombre=$_POST['nombre'];
+        $asunto=$_POST['asunto'];
+        $mensaje=$_POST['mensaje'];
+        $correo=$_POST['correo'];
+        $header = "FROM: noreply@example.com". "\r\n";
+        $header.="REPLY-TO: noreply@example.com". "\r\n";
+        $header.="X-Mailer: PHP/". phpversion();
+        $mail = @mail($correo,$asunto,$mensaje,$header);
+        if($mail){
+            echo "<h4> correo enviado</h4>";
+        }
     }
 }
 
