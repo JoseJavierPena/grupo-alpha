5bb2dd206c01e4cfd3b92c2ca41c5db1fcdffeb5
Aldayan <20187144@itla.edu.do>
2021-11-03 16:01:09 -0400

    back-end de evaluaciones individuales y arreglos de otras paginas

--- cuestionarios/evaluaciones individuales.php
+++ cuestionarios/evaluaciones individuales.php
@@ -2,22 +2,22 @@
 $conexion=mysqli_connect("localhost:8111","root","","pasantes alpha db");
 
 
-if(isset($_POST['insertar'])){
 $question1 = $_POST['question1'];
 $question2 = $_POST['question2'];
 $question3 = $_POST['question3'];
 $question4 = $_POST['question4'];
 $question5 = $_POST['question5'];
 $question6 = $_POST['question6'];
+$Id_formulario= $_POST["Id_formulario"];
 
 
 
-}
+
 
 
     
 
-$insertar="INSERT INTO individuales(conociminetoshtml,conociminetoscss,conocimientosjavascript,conocimientosphp,conocmientossqlmysql,otroslenguajes) VALUES('$question1','$question2','$question3','$question4','$question5','$question6')";
+$insertar="INSERT INTO individuales(conociminetoshtml,conociminetoscss,conocimientosjavascript,conocimientosphp,conocmientossqlmysql,otroslenguajes,Id_formulario) VALUES('$question1','$question2','$question3','$question4','$question5','$question6','$Id_formulario')";
 $resultado= mysqli_query($conexion,$insertar);
 
 
