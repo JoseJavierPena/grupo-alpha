10e86a20672cb2e7c809f268837b64f65f1214a9
Aldayan <20187144@itla.edu.do>
2021-11-06 23:19:09 -0400

    font y back end del chat

---
+++ admin/chat/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$dbHost = 'localhost:8111';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbDatabase = 'pasantes alpha db';
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase);
