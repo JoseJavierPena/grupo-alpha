5b5726ef2a74af3a95fee0fe59e50acc5a6936cf
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2021-11-25 16:26:14 -0400

    cambios para validar los campos

--- cuestionarios/Formulario.php
+++ cuestionarios/Formulario.php
@@ -10,14 +10,27 @@ $direccion=$_POST['direccion'];
 $donde_vives=$_POST['donde_vives'];
 $correo=$_POST['correo'];
 //validando datos
-$conexion=mysqli_connect("localhost:8111","root","","pasantes alpha db");
-$validar="SELECT * FROM formulario WHERE cedula='$cedula'|| telefono='$telefono'";
-$validando=$conexion->query($validar);
-if($validando->num_rows>0){
-    echo'<script type="text/javascript">
-    alert("Mensaje la cedula o el telefono ya estan registrados");
-       window.location.href="/grupo-alpha/cuestionatios/Formulario2.php"
-    </script>';
+//$conexion=mysqli_connect("localhost:8111","root","","pasantes alpha db");
+//$validar="SELECT * FROM formulario WHERE cedula='$cedula'|| telefono='$telefono'";
+//$validando=$conexion->query($validar);
+//if($validando->num_rows>0){
+    //echo'<script type="text/javascript">
+    //alert("Mensaje la cedula o el telefono ya estan registrados");
+       //window.location.href="/grupo-alpha/cuestionatios/Formulario2.php"
+    //</script>';
+//}
+$campo = array();
+if(strlen($cedula)!=11){
+    array_push($campo,"la cedula debe tener 11 caracteres");
+}
+if(strlen($telefono)!=10){
+    array_push($campo,"el telefono debe tener 10 caracteres");
+}
+if(count($campo)>0){
+    echo"<div class='error'>";
+    for($i = 0; $i < count($campo); $i++ ){
+        echo"<li>".$campo[$i]."</i>";
+    }
 }else{
 
 
